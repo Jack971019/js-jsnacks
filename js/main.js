b3bab4339dbb2ac4ac3b162faca900ccf7d2b532
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-06 15:08:05 +0200

    prompt how many numbers and cicle promt

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+/*Milestone 1:
+Crea una funzione chiamata sommaNumeri che prenda un array di N numeri e calcoli la somma di tutti questi numeri contenuti nell'array,
+ restituendolo come risultato.
+Utilizzare la funzione sommaNumeri per stampare il risultato.
+ L'array di N numeri da passare alla funzione lo create chiedendo all'utente quanti numeri vuole inserire (cioè N)
+ , e poi facendogli inserire questi N numeri all'utente stesso tramite apposito prompt().
+*/
+
+
+let arrayNumeri = [];
+let sommanumeriArray = arrayNumeri +     
+
+
+
+let n = parseInt(prompt("quanti numeri vuoi inserire?"));
+
+
+for(let i= 0; i < n ; i++){
+    let numeroUtente = parseInt(prompt("inserisci un numero"));
+    arrayNumeri.push(numeroUtente)
+
+}
+
+function
+
+
+
+
+
+
+
+
