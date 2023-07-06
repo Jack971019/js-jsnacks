6775d8cb551c63a39d7b8ac7f841dc989e04899b
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-06 17:52:41 +0200

    somma numeri completed

--- js/main.js
+++ js/main.js
@@ -8,25 +8,82 @@ Utilizzare la funzione sommaNumeri per stampare il risultato.
 */
 
 
+n = parseInt(prompt("quanti numeri vuoi inserire?"));
+
+
+
+
+
+
+
+
 let arrayNumeri = [];
- 
 
 
 
-let n = parseInt(prompt("quanti numeri vuoi inserire?"));
 
 
-for(let i= 0; i < n ; i++){
+
+
+for (let i = 0; i < n; i++) {
     let numeroUtente = parseInt(prompt("inserisci un numero"));
     arrayNumeri.push(numeroUtente)
+    
 
 }
 
-function sommaNumeri(arrayNumeri){
-    sommaNumeri = arrayNumeri + arrayNumeri[i]
+console.log(arrayNumeri);
+
+let sommaArray = sommaNumeri(arrayNumeri);
+console.log(sommaArray);
+
+
+let mediaArray = mediaArray(arrayNumeri);
+console.log(mediaArray);
+
+
+
+
+
+
+
+
+
+function sommaNumeri(arrayNumeri) {
+
+    let risultato = 0
+
+    for (let i = 0; i < arrayNumeri.length; i++) {
+
+        risultato = risultato + arrayNumeri[i];
+    }
+
+    return risultato ;
+
+
 }
 
-console.log(sommaNumeri);
+
+
+
+
+
+
+/*Crea una funzione chiamata mediaAritmetica che prende in input un array di N numeri e restituisca la media 
+aritmetica dei numeri contenuti nell'array.
+Utilizzare la funzione sommaNumeri precedentemente creata per calcolare la media dei numeri che vanno 
+chiesti all'utente attraverso dei prompt.
+Potete ad esempio provare ad usare questo programma per inserire N voti di scuola e farvi restituire 
+qual è la media di tutti i voti inseriti!
+*/
+
+
+
+
+
+
+
+
 
 
 
